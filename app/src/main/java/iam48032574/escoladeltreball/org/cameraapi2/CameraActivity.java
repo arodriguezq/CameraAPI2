98decd753d626ccd0e38b147fea8ffce731bbc74
arodriguezq <aitorrq@gmail.com>
2018-02-26 20:45:53 +0100

    Initial commit

---
+++ app/src/main/java/iam48032574/escoladeltreball/org/cameraapi2/CameraActivity.java
@@ -0,0 +1,18 @@
+package iam48032574.escoladeltreball.org.cameraapi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, PhotoFragment.newInstance())
+                    .commit();
+        }
+    }
+}
