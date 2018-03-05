b2c1059d74c16e2a3ad0d17bc114816bb46104d8
Aitor Rodríguez <aitorrq@gmail.com>
2018-03-05 14:29:56 +0100

    Preparing VideoFragment

--- app/src/main/java/iam48032574/escoladeltreball/org/cameraapi2/VideoFragment.java
+++ app/src/main/java/iam48032574/escoladeltreball/org/cameraapi2/VideoFragment.java
@@ -14,17 +14,7 @@ import android.support.v4.app.Fragment;
  * Use the {@link VideoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class VideoFragment extends Fragment
-{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class VideoFragment extends Fragment {
 
     public VideoFragment() {
     }
@@ -43,10 +33,7 @@ public class VideoFragment extends Fragment
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
